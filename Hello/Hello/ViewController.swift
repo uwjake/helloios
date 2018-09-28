4c208db4aa18d4852756b8111f345c9ef04fbd5f
Jake <37950768+uwjin@users.noreply.github.com>
2018-09-27 22:38:40 -0700

    initial commit

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Jake on 9/27/18.
+//  Copyright © 2018 Jake Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hello/HelloTests/HelloTests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloTests.swift
+//  HelloTests
+//
+//  Created by Jake on 9/27/18.
+//  Copyright © 2018 Jake Jin. All rights reserved.
+//
+
+import XCTest
+@testable import Hello
+
+class HelloTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
